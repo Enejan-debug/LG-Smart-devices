cd8d86c6526c59ab9a5b46a1e7aa7f989aa2a22e
Enejan-debug <enejanmirzayewa02@gmail.com>
2026-07-24 07:59:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ else to configure or deploy.
 from backend import create_app
 
 app = create_app()
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
